9fdd6ed90d39dc3b02e19d19e3be5ca2ffd19bb9
V, Murali (Tax&Accounting Prof) <Murali.V@thomsonreuters.com>
2024-12-17 19:09:31 +0530

    adding some comment in another file

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -8,6 +8,9 @@ namespace ApiForGitHubPractice
 
 		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public string? Summary { get; set; }
 	}
 }
